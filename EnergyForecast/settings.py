d583760ad3db8ca6966d062d2ddd94e0ab924dd2
DiaSarkar <59016478+DiaSarkar@users.noreply.github.com>
2020-03-08 11:49:39 +0000

    whitenoice middleware and staticfiles_storage

--- EnergyForecast/settings.py
+++ EnergyForecast/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'EnergyForecast.urls'
@@ -133,3 +134,4 @@ STATICFILES_DIRS = [
 ]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
