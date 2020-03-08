03de05e556ff0caf46145c598f76edbaa8ab1b91
DiaSarkar <59016478+DiaSarkar@users.noreply.github.com>
2020-03-08 11:11:24 +0000

    appended the static_url with energyforecast url

--- EnergyForecast/settings.py
+++ EnergyForecast/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = 'https://energyforecast.herokuapp.com/staticfiles/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
